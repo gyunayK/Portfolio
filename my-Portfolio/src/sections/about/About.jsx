bb8a2b01da64e5e2afa2ae017f51f7d25311946d
OdinOfRivia <gyunay@hotmail.dk>
2023-07-16 02:46:03 -0700

    Added -About me section

---
+++ my-Portfolio/src/sections/about/About.jsx
@@ -0,0 +1,33 @@
+const About = () => {
+  return (
+    <section>
+      <div className="w-full h-[700px]">
+        <div className="flex flex-col items-center justify-center h-full">
+          <div className="flex flex-col items-center justify-center gap-4 mt-8">
+            <div className="flex items-center justify-center gap-4">
+              <div>
+                <h1 className="text-7xl ">About Me</h1>
+                <p className="text-2xl max-w-[700px]">
+                  I am a Full Stack Developer who expertly combines
+                  professionalism with a dahs of creativity. I thrive on
+                  tackling challenges head-on, all while fostering a
+                  collaborative team environment. I am a self-motivated
+                  individual who is always looking to learn and grow.
+                </p>
+                <p className="text-2xl max-w-[700px]">
+                  My skill set spans a diverse range of front-end and back-end
+                  techonologies and frameworks.
+                </p>
+              </div>
+
+              <div className="w-[400px] h-[400px] bg-[#242222] rounded-full"></div>
+            </div>
+            <div className="flex items-center justify-center gap-4"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- my-Portfolio/src/sections/navbar/Navbar.jsx
+++ my-Portfolio/src/sections/navbar/Navbar.jsx
@@ -6,35 +6,45 @@ function Navbar() {
 
   return (
     <div className="fixed w-full h-full">
-      <button
-        className="p-10 mr-4  absolute top-10 right-5 z-10 sm:block md:hidden"
+      <div
+        id="nav-icon2"
+        className={`p-8 absolute top-10 right-5 z-10 sm:block md:hidden scale-75 ${
+          isMenuOpen ? "open" : ""
+        }`}
         onClick={() => setIsMenuOpen(!isMenuOpen)}
+        style={{ cursor: "pointer" }}
       >
-        <div id="nav-icon2" className={isMenuOpen ? "open" : ""}>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </button>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
 
       <div
-        className={`bg-white text-black w-screen h-full fixed top-0 right-0 transition-transform duration-[200ms] ease-in-out transform overflow-hidden ${
+        className={`bg-black text-white w-screen h-full fixed top-0 right-0 transition-transform duration-[200ms] ease-in-out transform overflow-hidden ${
           isMenuOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
         <ul className="text-4xl flex flex-col items-center justify-center space-y-4 h-full gap-8">
-          <li className="cursor-pointer">HOME </li>
-          <li className="cursor-pointer">ABOUT </li>
-          <li className="cursor-pointer">WORK </li>
-          <li className="cursor-pointer">CONTACT </li>
+          <li className="cursor-pointer" onClick={() => setIsMenuOpen(false)}>
+            HOME{" "}
+          </li>
+          <li className="cursor-pointer" onClick={() => setIsMenuOpen(false)}>
+            ABOUT{" "}
+          </li>
+          <li className="cursor-pointer" onClick={() => setIsMenuOpen(false)}>
+            WORK{" "}
+          </li>
+          <li className="cursor-pointer" onClick={() => setIsMenuOpen(false)}>
+            CONTACT{" "}
+          </li>
         </ul>
       </div>
 
       <div className="hidden md:block">
-        <ul className="text-4xl flex gap-7 content-end justify-end p-8 font-['NATS']">
+        <ul className="text-4xl text-[#000000] flex gap-7 content-end justify-end p-8 font-['NATS']">
           <li className="cursor-pointer border-transparent border-b-2 hover:border-white ">
             HOME
           </li>
