d64a3e3e5296c305c8a2220f2cad9fd42d787511
OdinOfRivia <gyunay@hotmail.dk>
2023-07-25 17:09:14 -0700

    ADDED -Imroved responsivnes across all devices -Added groundwork for the projects section

---
+++ my-Portfolio/src/sections/projects/Projects.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <section>
+      <div className="w-full h-full bg-[#ededed] py-32">
+        <div className="flex flex-col items-center justify-center ">
+          <div className=" flex flex-col items-center gap-4">
+            <h2 className="font-Tektur text-[#b931f8] text-lg font-bold tracking-wider">
+              WORKS
+            </h2>
+            <h1 className="text-5xl font-serif font-medium leading-8">
+              Latest Projects
+            </h1>
+            <h4 className="font-medium text-lg">Here you can find some of the projects i created recently </h4>
+          </div>
+            
+           
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
