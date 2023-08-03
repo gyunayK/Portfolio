58094a57b233e21ee54d2d7c7c7e03ef274ee28e
OdinOfRivia <gyunay@hotmail.dk>
2023-08-03 15:15:32 -0700

    Update -Removed overused projects -Added google Analytics

--- my-Portfolio/src/sections/projects/projectsList.js
+++ my-Portfolio/src/sections/projects/projectsList.js
@@ -6,40 +6,7 @@ export const projects = [
             "It's a place where movie lovers can discover new films, delve into details about each one, and save their favorites to return to later.",
         techStack:
             "React, Node.js, MongoDB, TailwindCSS, API, Styled-components, JSON Web Token, bcrypt.",
-        liveLink: "https://movie-search-client-lac.vercel.app/",
-        githubLink: "https://github.com/gyunayK/MovieSearch",
-        projectIMG:
-            "https://ik.imagekit.io/riviaa/Portfolio/Group%202%20(1).png?updatedAt=1690456945715",
-    },
-    {
-        title: "Movie Deck",
-        description:
-            "It's a place where movie lovers can discover new films, delve into details about each one, and save their favorites to return to later.",
-        techStack:
-            "React, Node.js, MongoDB, TailwindCSS, API, Styled-components, JSON Web Token, bcrypt.",
-        liveLink: "https://movie-search-client-lac.vercel.app/",
-        githubLink: "https://github.com/gyunayK/MovieSearch",
-        projectIMG:
-            "https://ik.imagekit.io/riviaa/Portfolio/Group%202%20(1).png?updatedAt=1690456945715",
-    },
-    {
-        title: "Movie Deck",
-        description:
-            "It's a place where movie lovers can discover new films, delve into details about each one, and save their favorites to return to later.",
-        techStack:
-            "React, Node.js, MongoDB, TailwindCSS, API, Styled-components, JSON Web Token, bcrypt.",
-        liveLink: "https://movie-search-client-lac.vercel.app/",
-        githubLink: "https://github.com/gyunayK/MovieSearch",
-        projectIMG:
-            "https://ik.imagekit.io/riviaa/Portfolio/Group%202%20(1).png?updatedAt=1690456945715",
-    },
-    {
-        title: "Movie Deck",
-        description:
-            "It's a place where movie lovers can discover new films, delve into details about each one, and save their favorites to return to later.",
-        techStack:
-            "React, Node.js, MongoDB, TailwindCSS, API, Styled-components, JSON Web Token, bcrypt.",
-        liveLink: "https://movie-search-client-lac.vercel.app/",
+        liveLink: "https://movie-deck-client.vercel.app/",
         githubLink: "https://github.com/gyunayK/MovieSearch",
         projectIMG:
             "https://ik.imagekit.io/riviaa/Portfolio/Group%202%20(1).png?updatedAt=1690456945715",
