ea4271265f04af5e2256e0011afededcac98065f
Gyunay Kadirov <gyunay@hotmail.dk>
2024-01-22 16:01:31 -0800

    refactor: relocate timeline events  for code clarity

--- my-Portfolio/src/sections/timeline/Timeline.jsx
+++ my-Portfolio/src/sections/timeline/Timeline.jsx
@@ -1,49 +1,8 @@
 /* eslint-disable react/prop-types */
-import { FaReact, FaBriefcase } from "react-icons/fa";
 import { TbTilde } from "react-icons/tb";
-import { MdSchool } from "react-icons/md";
+import { timelineEvents } from './timelineEvents';
 
-const items = [
-  {
-    date: "Aug 2022",
-    endDate: "Aug 2023",
-    cardTitle: "Web and Mobile Application Development Diploma",
-    cardDetailedText: "Cornerstone International Community College of Canada",
-    icon: <MdSchool />,
-  },
-  {
-    date: "Jun 2023",
-    endDate: "Jul 2023",
-    cardTitle: "Movie Deck - Full Stack App",
-    cardDetailedText:
-      "This movie application offers a platform for discovering and discussing films, with features for users to find new titles, learn about them, and curate a list of favorites. It also includes a comment system for community interaction. Designed for ease of use, it works seamlessly across devices. Future enhancements will add personalized recommendations and user-generated reviews.",
-    icon: <FaReact />,
-  },
-  {
-    date: "Aug 2023",
-    endDate: "Oct 2023",
-    cardTitle: "Kadirov - Full Stack App",
-    cardDetailedText:
-      "Kadirov: Your fashion-forward marketplace offers a curated selection of clothing and unique makeup accessories. Users can effortlessly filter, search, and favorite items. With secure Stripe checkout and personalized shopping features, it provides a seamless and enjoyable online shopping experience.",
-    icon: <FaReact />,
-  },
-  {
-    date: "Aug 2023",
-    endDate: "Dec 2023",
-    cardTitle: "Front-end Developer",
-    cardDetailedText:
-      "At eEpic Technologies, I crafted responsive web solutions using Next.js and TypeScript, focusing on mobile-first design. My integration of diverse APIs enhanced the app's dynamic content and user engagement. I also improved performance by 15% with advanced GraphQL queries. Collaboratively, I helped integrate front and backend components, boosting team synergy and communication.",
-    icon: <FaBriefcase />,
-  },
-  {
-    date: "Aug 2022",
-    endDate: "Present",
-    cardTitle: "Front-end Developer",
-    cardDetailedText:
-      "At Summations, I improved SEO and website performance through Next.js 14 for server-side rendering and mobile responsiveness. I reduced load times by 30% with asset optimization and code splitting. I also developed seven custom React hooks for better code efficiency and integrated custom Mantine UI components for an enhanced user experience, especially for academic users.",
-    icon: <FaBriefcase />,
-  },
-];
+
 
 const TimelineItem = ({ item, alignment }) => {
   const isLeftAligned = alignment === "left";
@@ -119,7 +78,7 @@ export default function Timeline() {
             {/* Vertical line */}
             <div className="absolute hidden w-1 h-full transform -translate-x-1/2 bg-[#c9e2f5]  lg:block left-1/2"></div>
             {/* Items */}
-            {items.map((item, index) => (
+            {timelineEvents.map((item, index) => (
               <TimelineItem
                 key={index}
                 item={item}
---
+++ my-Portfolio/src/sections/timeline/timelineEvents.jsx
@@ -0,0 +1,44 @@
+import { FaReact, FaBriefcase } from "react-icons/fa";
+import { MdSchool } from "react-icons/md";
+
+export const timelineEvents = [
+  {
+    date: "Aug 2022",
+    endDate: "Aug 2023",
+    cardTitle: "Web and Mobile Application Development Diploma",
+    cardDetailedText: "Cornerstone International Community College of Canada",
+    icon: <MdSchool />,
+  },
+  {
+    date: "Jun 2023",
+    endDate: "Jul 2023",
+    cardTitle: "Movie Deck - Full Stack App",
+    cardDetailedText:
+      "This movie application offers a platform for discovering and discussing films, with features for users to find new titles, learn about them, and curate a list of favorites. It also includes a comment system for community interaction. Designed for ease of use, it works seamlessly across devices. Future enhancements will add personalized recommendations and user-generated reviews.",
+    icon: <FaReact />,
+  },
+  {
+    date: "Aug 2023",
+    endDate: "Oct 2023",
+    cardTitle: "Kadirov - Full Stack App",
+    cardDetailedText:
+      "Kadirov: Your fashion-forward marketplace offers a curated selection of clothing and unique makeup accessories. Users can effortlessly filter, search, and favorite items. With secure Stripe checkout and personalized shopping features, it provides a seamless and enjoyable online shopping experience.",
+    icon: <FaReact />,
+  },
+  {
+    date: "Aug 2023",
+    endDate: "Dec 2023",
+    cardTitle: "Front-end Developer",
+    cardDetailedText:
+      "At eEpic Technologies, I crafted responsive web solutions using Next.js and TypeScript, focusing on mobile-first design. My integration of diverse APIs enhanced the app's dynamic content and user engagement. I also improved performance by 15% with advanced GraphQL queries. Collaboratively, I helped integrate front and backend components, boosting team synergy and communication.",
+    icon: <FaBriefcase />,
+  },
+  {
+    date: "Aug 2022",
+    endDate: "Present",
+    cardTitle: "Front-end Developer",
+    cardDetailedText:
+      "At Summations, I improved SEO and website performance through Next.js 14 for server-side rendering and mobile responsiveness. I reduced load times by 30% with asset optimization and code splitting. I also developed seven custom React hooks for better code efficiency and integrated custom Mantine UI components for an enhanced user experience, especially for academic users.",
+    icon: <FaBriefcase />,
+  },
+];
