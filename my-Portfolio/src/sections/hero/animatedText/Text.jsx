75bde2e8f376eb4bb9a316b74790994d9eb5898f
Gyunay Kadirov <gyunay@hotmail.dk>
2024-01-22 16:28:26 -0800

    style: change hero section heading to 'Software Developer'

--- my-Portfolio/src/sections/hero/animatedText/Text.jsx
+++ my-Portfolio/src/sections/hero/animatedText/Text.jsx
@@ -3,7 +3,7 @@ import "./animation.css";
 function Text() {
   return (
     <h2 className=" mt-4 md:mt-0 md:text-3xl text-white mb-1 font-Tektur animatedText">
-      I am a Web Developer
+      I am a Software Developer
     </h2>
   );
 }
