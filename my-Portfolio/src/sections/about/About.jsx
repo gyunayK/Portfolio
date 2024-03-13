03576830331604e84c9406833ce533d05230671e
Gyunay Kadirov <gyunay@hotmail.dk>
2024-03-13 00:59:00 -0700

    update: Revise About section for clarity and impact

--- my-Portfolio/src/sections/about/About.jsx
+++ my-Portfolio/src/sections/about/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
                 ABOUT
               </h2>
             </Reveal>
-            <Reveal width='p-3 -m-3'>
+            <Reveal width="p-3 -m-3">
               <h1 className="text-5xl font-serif font-medium leading-8">
                 Personal Info
               </h1>
@@ -30,32 +30,30 @@ const About = () => {
               <Reveal width="w-fit">
                 <h1 className="text-2xl font-semibold">Hey there!</h1>
               </Reveal>
-
-              <Reveal width="w-full">
-                <p className="mt-4">
-                  I&apos;m a software developer with a rich background in
-                  front-end and back-end technologies. I love crafting robust
-                  web applications that perform exceptionally and provide
-                  enjoyable user experiences. In each project, I aim to blend
-                  functional beauty with technical efficiency, creating super
-                  responsive and user-friendly applications.
+              <Reveal width="mt-4">
+                <p>
+                  I am a software developer specializing in both front-end and
+                  back-end development. My passion lies in crafting web
+                  applications that stand out for their performance and
+                  user-centric design. Each project is an opportunity to blend
+                  innovative functionality with aesthetic appeal, ensuring a
+                  seamless and engaging user experience.
                 </p>
               </Reveal>
-              <Reveal width="w-full">
-                <p className="mt-10">
-                  I&apos;m really into optimizing application performance and
-                  making sure that everything is speedy, responsive, and
-                  efficient. I&apos;m all about embracing technical excellence
-                  and working collaboratively with others to bring high-quality,
-                  meaningful software to life. My ultimate goal is to build
-                  scalable web applications that are not only effective but also
-                  a joy to use both on the client side and server side.
+              <Reveal width="mt-10">
+                <p>
+                  In my work, I focus on the nuts and bolts of application
+                  performance, striving for responsiveness and efficiency. My
+                  goal is to create software that is not just powerful and
+                  reliable, but also intuitive and enjoyable to use.
+                  Collaborating with teams to bring high-quality, scalable
+                  applications to life is what drives me in the tech industry.
                 </p>
               </Reveal>
             </div>
             <div className="w-full max-w-[500px]">
               <Reveal>
-              <h1 className="text-2xl font-semibold">Skills</h1>
+                <h1 className="text-2xl font-semibold">Skills</h1>
               </Reveal>
               <div className="mt-4">
                 <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 gap-10 text-white">
