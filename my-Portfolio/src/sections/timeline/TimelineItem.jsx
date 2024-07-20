840bdad30b17d3fb40d1159da6ad4a11a0855bbb
Gyunay Kadirov <gyunay@hotmail.dk>
2024-07-20 14:50:47 -0700

    fix: :bug: timeline items not rendering properly
    A div which renderes on the top left / right side of each timeline item was not rendering because of the Reveal copmonent

--- my-Portfolio/src/sections/timeline/TimelineItem.jsx
+++ my-Portfolio/src/sections/timeline/TimelineItem.jsx
@@ -12,41 +12,37 @@ export default function TimelineItem({ item, alignment }) {
       <div
         className={`w-full lg:w-1/2 ${isLeftAligned ? 'lg:pr-8' : 'lg:pl-8'}`}
       >
-        <Reveal width={'w-fit'}>
-          <div className="relative flex-1 mb-10 bg-white rounded shadow lg:mb-8 ">
+        <Reveal width={'w-fit px-4'}>
+        <div className="relative flex-1 mb-10 bg-white rounded shadow lg:mb-8 ">
+          <div
+            className={`absolute inline-block w-4 overflow-hidden -translate-y-1/2 ${
+              isLeftAligned ? 'top-3 -right-4' : 'top-7 -left-4'
+            }`}
+          >
             <div
-              className={`absolute bg-black inline-block w-4 overflow-hidden -translate-y-1/2 ${
-                isLeftAligned ? 'top-3 -right-4' : 'top-7 -left-4'
-              }`}
-            >
-              <div
-                className={`absolute h-10 ${
-                  isLeftAligned ? 'origin-bottom-left' : 'origin-top-right'
-                } transform -rotate-45 bg-black shadow z-[1001] `}
-              ></div>
+              className={`hidden h-10 ${
+                isLeftAligned ? 'origin-bottom-left' : 'origin-top-right'
+              } transform -rotate-45 bg-white shadow lg:block `}
+            ></div>
+          </div>
+          <div className="relative z-20 flex flex-col sm:flex-row items-center">
+            <div className="sm:p-4 pt-6 md:w-1/5 h-full flex gap-x-3 sm:flex-col text-center items-center">
+              <span className="text-lg text-gray-700 ">{item.date}</span>
+              {item.endDate && (
+                <>
+                  <TbTilde className="text-lg text-gray-700" />
+                  <span className="text-lg text-gray-700 ">{item.endDate}</span>
+                </>
+              )}
             </div>
-            <div className="relative z-20 flex flex-col sm:flex-row items-center">
-              <div className="sm:p-4 pt-6 md:w-1/5 h-full flex gap-x-3 sm:flex-col text-center items-center">
-                <span className="text-lg text-gray-700 ">{item.date}</span>
-                {item.endDate && (
-                  <>
-                    <TbTilde className="text-lg text-gray-700" />
-                    <span className="text-lg text-gray-700 ">
-                      {item.endDate}
-                    </span>
-                  </>
-                )}
-              </div>
-              <div className="flex-1 p-4 pr-4 border-l border-gray-300">
-                <p className="text-2xl font-bold text-gray-700 text-bold text-center sm:text-start">
-                  {item.jobTitle}
-                </p>
-                <p className="text-gray-700 mt-2 text-start">
-                  {item.jobDetails}
-                </p>
-              </div>
+            <div className="flex-1 p-4 pr-4 border-l border-gray-300">
+              <p className="text-2xl font-bold text-gray-700 text-bold text-center sm:text-start">
+                {item.jobTitle}
+              </p>
+              <p className="text-gray-700 mt-2 text-start">{item.jobDetails}</p>
             </div>
           </div>
+        </div>
         </Reveal>
       </div>
       <div
