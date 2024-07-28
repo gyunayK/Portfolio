c02b18eadb57134d6299cc8f2ba30353bef6f5e4
Gyunay Kadirov <gyunay@hotmail.dk>
2024-07-27 23:28:37 -0700

    style: :art: fix indentation and removed traling commas

--- my-Portfolio/src/sections/timeline/timelineEvents.jsx
+++ my-Portfolio/src/sections/timeline/timelineEvents.jsx
@@ -1,59 +1,59 @@
-import { FaReact, FaBriefcase } from "react-icons/fa";
-import { MdSchool } from "react-icons/md";
+import { FaReact, FaBriefcase } from 'react-icons/fa'
+import { MdSchool } from 'react-icons/md'
 
 export const TimelineEvents = [
   {
-    date: "Aug 2022",
-    endDate: "Dec 2024",
-    jobTitle: "Web and Mobile Application Development Diploma",
-    jobDetails: "Cornerstone International Community College of Canada",
-    icon: <MdSchool />,
+    date: 'Aug 2022',
+    endDate: 'Dec 2024',
+    jobTitle: 'Web and Mobile Application Development Diploma',
+    jobDetails: 'Cornerstone International Community College of Canada',
+    icon: <MdSchool />
   },
   {
-    date: "Jun 2023",
-    endDate: "Jul 2023",
-    jobTitle: "Movie Deck - Full Stack App",
+    date: 'Jun 2023',
+    endDate: 'Jul 2023',
+    jobTitle: 'Movie Deck - Full Stack App',
     jobDetails:
-      "This movie application offers a platform for discovering and discussing films, with features for users to find new titles, learn about them, and curate a list of favorites. It also includes a comment system for community interaction. Designed for ease of use, it works seamlessly across devices. Future enhancements will add personalized recommendations and user-generated reviews.",
-    icon: <FaReact />,
+      'This movie application offers a platform for discovering and discussing films, with features for users to find new titles, learn about them, and curate a list of favorites. It also includes a comment system for community interaction. Designed for ease of use, it works seamlessly across devices. Future enhancements will add personalized recommendations and user-generated reviews.',
+    icon: <FaReact />
   },
   {
-    date: "Aug 2023",
-    endDate: "Oct 2023",
-    jobTitle: "Kadirov - Full Stack App",
+    date: 'Aug 2023',
+    endDate: 'Oct 2023',
+    jobTitle: 'Kadirov - Full Stack App',
     jobDetails:
-      "Kadirov: Your fashion-forward marketplace offers a curated selection of clothing and unique makeup accessories. Users can effortlessly filter, search, and favorite items. With secure Stripe checkout and personalized shopping features, it provides a seamless and enjoyable online shopping experience.",
-    icon: <FaReact />,
+      'Kadirov: Your fashion-forward marketplace offers a curated selection of clothing and unique makeup accessories. Users can effortlessly filter, search, and favorite items. With secure Stripe checkout and personalized shopping features, it provides a seamless and enjoyable online shopping experience.',
+    icon: <FaReact />
   },
   {
-    date: "Jun 2023",
-    endDate: "Dec 2023",
-    jobTitle: "Front-end Developer",
+    date: 'Jun 2023',
+    endDate: 'Dec 2023',
+    jobTitle: 'Front-end Developer',
     jobDetails:
       "At eEpic Technologies, I crafted responsive web solutions using Next.js and TypeScript, focusing on mobile-first design. My integration of diverse APIs enhanced the app's dynamic content and user engagement. I also improved performance by 15% with advanced GraphQL queries. Collaboratively, I helped integrate front and backend components, boosting team synergy and communication.",
-    icon: <FaBriefcase />,
+    icon: <FaBriefcase />
   },
   {
-    date: "Aug 2022",
-    endDate: "Apr 2024",
-    jobTitle: "Front-end Developer",
+    date: 'Aug 2022',
+    endDate: 'Apr 2024',
+    jobTitle: 'Front-end Developer',
     jobDetails:
-      "At Summations, I improved SEO and website performance through Next.js 14 for server-side rendering and mobile responsiveness. I reduced load times by 30% with asset optimization and code splitting. I also developed seven custom React hooks for better code efficiency and integrated custom Mantine UI components for an enhanced user experience, especially for academic users.",
-    icon: <FaBriefcase />,
+      'At Summations, I improved SEO and website performance through Next.js 14 for server-side rendering and mobile responsiveness. I reduced load times by 30% with asset optimization and code splitting. I also developed seven custom React hooks for better code efficiency and integrated custom Mantine UI components for an enhanced user experience, especially for academic users.',
+    icon: <FaBriefcase />
   },
   {
-    date: "Dec 2022",
-    endDate: "Apr 2024",
-    jobTitle: "Full-stack Developer",
+    date: 'Dec 2022',
+    endDate: 'Apr 2024',
+    jobTitle: 'Full-stack Developer',
     jobDetails:
-      "At Waniskāw Foundation, I improved the technology infrastructure and user interface. I used Node.js for efficient server-side processes and AWS S3 for data handling. I optimized the interface design with SASS and integrated MongoDB to improve scalability and data management.",
-    icon: <FaBriefcase />,
+      'At Waniskāw Foundation, I improved the technology infrastructure and user interface. I used Node.js for efficient server-side processes and AWS S3 for data handling. I optimized the interface design with SASS and integrated MongoDB to improve scalability and data management.',
+    icon: <FaBriefcase />
   },
   {
-    date: "Apr 2024",
-    endDate: "Present",
-    jobTitle: "Software Developer Intern @ IcePanel",
-    jobDetails: "",
-    icon: <FaBriefcase />,
+    date: 'Apr 2024',
+    endDate: 'Present',
+    jobTitle: 'Software Developer Intern @ IcePanel',
+    jobDetails: '',
+    icon: <FaBriefcase />
   }
-];
+]
