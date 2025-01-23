1ddb6597a581abe0c40848be264147fd548c1059
Gyunay Kadirov <gyunay@hotmail.dk>
2025-01-23 02:52:06 -0800

    feat: update footer

--- my-Portfolio/src/sections/footer/Footer.jsx
+++ my-Portfolio/src/sections/footer/Footer.jsx
@@ -23,7 +23,7 @@ function Footer() {
           </a>
         </div>
         <div className="pt-1.5 pb-5 opacity-100 flex items-center justify-center gap-x-2">
-          <span className="text-white opacity-80">Created in beautiful British Columbia</span>
+          <span className="text-white opacity-80">Developed in beautiful British Columbia</span>
           <img src="/img/canada.svg" alt="canada-flag" width={20} height={20} />
         </div>
       </div>
